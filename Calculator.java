4aa8bdda42440cbd6cb2595d39b33d0f6f8c9488
wulinbaba <34059956+wulinbaba@users.noreply.github.com>
2024-09-08 23:39:36 +0800

    Update Calculator.java
    Update undo/redo method of Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,106 +1,56 @@
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class Calculator extends BasicCalculator{
-
-    private List<BigDecimal> lastNumList = new ArrayList<>(); // 保存最近系列操作值
-    private List<String> lastOptList = new ArrayList<>(); // 保存最近系列运算符
-    private List<BigDecimal> lastTotalList = new ArrayList<>(); // 保存最近系列结果值
-    private int lastOptIndex = -1; // undo/redo最近操作索引
-    private int validIndexMax = -1; // undo/redo有效索引最大值
-
-    /**
-     * 计算操作，等同于按下计算器的等于按钮
-     */
-    public void calc(){
-        preNum = getPreNum() == null ? BigDecimal.ZERO : getPreNum();
-        if(operator == null){
-            System.out.println("请输入运算符!");
-        }
-        if(newNum != null){ // 已输入新值
-            // 累加计算，调用基类计算方法
-            BigDecimal ret = calculate(preNum, operator, newNum);
-            if(this.lastOptIndex == -1){ // 未执行redo/undo处于中间过程
-                lastTotalList.add(preNum);
-                lastNumList.add(newNum);
-                lastOptList.add(operator);
-            }else{ // 执行过redo/undo操作,覆盖undo/redo操作记录,并记录有效索引最大值
-                this.lastOptIndex++;
-                this.validIndexMax = this.lastOptIndex;
-                this.lastTotalList.set(this.lastOptIndex, ret);
-                this.lastNumList.set(this.lastOptIndex-1, newNum);
-                this.lastOptList.set(this.lastOptIndex-1, operator);
-            }
-            preNum = ret;
-            operator = null;
-            newNum = null;
-        }
-        display(); // 显示操作结果
-    }
+    private Stack<BigDecimal> undoStack = new Stack<>(); // 撤销栈
+    private Stack<BigDecimal> redoStack = new Stack<>(); // 重做栈
 
     /**
      * 撤销操作，回撤到上一个计算结果
      */
     public void undo(){
-        if(preNum != null && lastOptIndex == -1){ // 未进行undo/redo操作,存储最后计算结果
-            lastTotalList.add(preNum);
-            operator = null;
-            newNum = null;
-        }
-
-        if(lastTotalList.size() == 0){
-            System.out.println("无操作，无法继续撤销!");
-        }else if(lastTotalList.size() == 1){
-            System.out.println("undo后值:0,"+"undo前值:"+preNum);
-            preNum = BigDecimal.ZERO;
+        if(undoStack.isEmpty()){
+            System.out.println("无法再undo!");
         } else {
-            if(lastOptIndex == -1){
-                lastOptIndex = lastOptList.size()-1;
-            }else{
-                if(lastOptIndex-1 < 0){
-                    System.out.println("无法再undo!");
-                    return;
-                }
-                lastOptIndex--;
-            }
-            undoOperate(lastTotalList.get(lastOptIndex),lastOptList.get(lastOptIndex), lastNumList.get(lastOptIndex));
+            BigDecimal lastNum = undoStack.pop();
+            redoStack.push(preNum);
+            preNum = lastNum;
+            System.out.println("undo撤销操作后的值：" + preNum);
         }
-        display(); // 显示操作结果
     }
 
     /**
      * 重做操作，根据撤销进行重做上一个操作
      */
     public void redo(){
-        try{
-            if(lastOptIndex > -1){
-                if(lastOptIndex + 1 == lastTotalList.size() || lastOptIndex+1 == this.validIndexMax+1){
-                    System.out.println("无法再redo!");
-                    return;
-                }
-                lastOptIndex++;
-
-                redoOperate(lastTotalList.get(lastOptIndex),lastOptList.get(lastOptIndex-1), lastNumList.get(lastOptIndex-1));
-            }
-            display(); // 显示操作结果
-        }catch (Exception e){
-            System.out.println("redo异常,lastOptIndex:"+lastOptIndex);
+        if(redoStack.isEmpty()){
+            System.out.println("无法再redo!");
+        } else {
+            BigDecimal nextNum = redoStack.pop();
+            undoStack.push(nextNum);
+            preNum = nextNum;
+            System.out.println("redo重做操作后的值：" + preNum);
         }
     }
 
-    private void undoOperate(BigDecimal lastTotal, String lastOpt, BigDecimal lastNum) {
-        System.out.println("undo后值:"+lastTotal+",undo前值:"+preNum+",undo的操作:"+lastOpt+",undo操作的值:"+lastNum);
-        preNum = lastTotal;
-        operator = null;
-        newNum = null;
-    }
-
-    private void redoOperate(BigDecimal redoTotal, String redoOpt, BigDecimal redoNum) {
-        System.out.println("redo后值:"+redoTotal+",redo前值:"+preNum+",redo的操作:"+redoOpt+",redo操作的值:"+redoNum);
-        preNum = redoTotal;
-        operator = null;
-        newNum = null;
+    /**
+     * 计算操作，等同于按下计算器的等于按钮
+     */
+    public void calc(){
+        preNum = getPreNum() == null ? BigDecimal.ZERO : getPreNum();
+        undoStack.push(preNum);
+        if(operator == null){
+            System.out.println("请输入运算符!");
+        }
+        if(newNum != null){ // 已输入新值
+            // 累加计算，调用基类计算方法
+            BigDecimal ret = calculate(preNum, operator, newNum);
+            preNum = ret;
+            operator = null;
+            newNum = null;
+            redoStack.clear(); // 清空重做栈
+        }
+        display(); // 显示操作结果
     }
 
     /**
